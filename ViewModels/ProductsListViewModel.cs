fe0a78e2607e66901967370672d7182d03e81f80
DmitryFetisov <118624548+DmitryFetisov@users.noreply.github.com>
2023-02-06 15:03:59 +0300

    Add files via upload

---
+++ ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using ProductStore.Data.Models;
+
+namespace ProductStore.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> allProducts { get; set; } = null!;
+        public IEnumerable<Store> allStores { get; set; } = null!;
+    }
+}
---
+++ ViewModels/ShopCartViewModels.cs
@@ -0,0 +1,9 @@
+﻿using ProductStore.Data.Models;
+
+namespace ProductStore.ViewModels
+{
+    public class ShopCartViewModels
+    {
+        public ShopCart ShopCart { get; set; }
+    }
+}
